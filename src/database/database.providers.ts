6ec08d92efa4640f0c28d6bba01f1c20854a19cc
Nhahan <kisy324@naver.com>
2023-11-17 14:57:44 +0900

    feat: db, typeorm base settings

---
+++ src/database/database.providers.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+
+const DB = Symbol('DATA_SOURCE');
+
+export const databaseProviders = [
+  {
+    provide: DB,
+    useFactory: async (configService: ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
