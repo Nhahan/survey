bf1d0a23b6fb326397460880388c17f2fda9c4fb
Nhahan <kisy324@naver.com>
2023-11-18 00:49:25 +0900

    chore: docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { LoggerModule } from './common/logger/logger.module';
+import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {LoggerModule} from './common/logger/logger.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { ormConfig } from './configs/orm.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), LoggerModule.forRoot()],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), LoggerModule.forRoot(), TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ormConfig(configService),
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
